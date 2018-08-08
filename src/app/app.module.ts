ab042a1c3d7b84046d4edc0668c73d1caab97f92
Joseph Higaki <josephhigaki@hotmail.com>
2018-08-07 22:40:49 -0500

    add bike binding and edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BikesComponent } from './bikes/bikes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
