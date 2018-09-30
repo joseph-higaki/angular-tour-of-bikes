dd26424bf1d077d97c0b9bf84cc95fef0971ab3a
Joseph Higaki <josephhigaki@hotmail.com>
2018-09-30 00:40:17 -0500

    bike list and selection

---
+++ src/app/mock-bikes.ts
@@ -0,0 +1,11 @@
+import {Bike} from './bike';
+
+export const BIKES: Bike[] = [
+    {id:1, name: "devinci"},
+    {id:2, name: "yt"},
+    {id:3, name: "santa cruz"},
+    {id:4, name: "transition"},
+    {id:5, name: "specialized"},
+    {id:6, name: "giant"},
+    {id:7, name: "pivot"}
+];
