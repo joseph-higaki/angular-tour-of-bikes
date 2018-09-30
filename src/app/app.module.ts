c8c382e39cd0e44652d28002de076d1d4ba18227
Joseph Higaki <josephhigaki@hotmail.com>
2018-09-30 01:26:23 -0500

    bike detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BikesComponent } from './bikes/bikes.component';
+import { BikeDetailComponent } from './bike-detail/bike-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BikesComponent
+    BikesComponent,
+    BikeDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bike-detail/bike-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Bike} from '../bike';
+
+@Component({
+  selector: 'app-bike-detail',
+  templateUrl: './bike-detail.component.html',
+  styleUrls: ['./bike-detail.component.css']
+})
+
+
+
+export class BikeDetailComponent implements OnInit {
+  @Input() bike: Bike;  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
